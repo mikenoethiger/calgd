5e038611b71f0fa16deb287226334be44a74483d
Mike Nöthiger <noethiger.mike@gmail.com>
2019-11-15 10:02:51 +0100

    clean up code, document code

--- heap/tree_heap.h
+++ heap/tree_heap.h
@@ -1,8 +1,10 @@
-
-
-/* 
- * A max-heap is a priority queue, which means it's a queue
- * that always returns the element with the highest priority.
+/* Author: Mike Nöthiger
+ *
+ * This is the interface definition of a max-heap.
+ *
+ * A max-heap is a priority queue, i.e. the queue
+ * always delivers the element with the highest priority
+ * in the queue.
  *
  * Max-Heap conditions:
  * 1. Its structure is an almost complete binary tree
@@ -18,38 +20,62 @@ typedef struct heap_element {
     struct heap_element *right;
 } HeapElement;
 
+/*
+ * Data structure for a tree heap.
+ * 
+ * size: Represents the amount of heap elements present in the tree.
+ * max_element: A pointer to the max element (based on its key) is maintained.
+ * The max element represents the root of the tree.
+ * last_element: A pointer to the last inserted element is maintained.
+ * Once a heap element is inserted into the tree, its place will never 
+ * be changed unless it is removed again.
+ * The only possible change in the tree structure is to swap keys of 
+ * two heap elements.
+ */
 typedef struct heap {
     size_t size;
     HeapElement *max_element;
     HeapElement *last_element;    
 } Heap;
 
+/*
+ * Initializes the heap datastructure and its memory.
+ * The heap is initialized with a size of 0.
+ */
 Heap *heap_create();
+
+/*
+ * Destroys the heap data strucuture and its memory.
+ */
 void heap_destroy(Heap *heap);
 
 /* 
- * Get the max element of the heap (based on key), 
- * without removing the element.
+ * Get the max element of the heap (based on key). 
+ * Does not change the heap. 
  * O(1)
  *
- * returns max element or 0 if the heap is empty.
+ * returns key of max element or 0 if the heap is empty.
  */
 int heap_get_max(Heap *heap);
 
 /*
- * Get and remove the max element of the heap (based on key).
+ * Removes the max element of the heap (based on key).
  *
- * returns max element or 0 if the heap is empty.
+ * returns key of removed element or 0 if the heap is empty.
  */
 int heap_remove_max(Heap *heap);
 
 /*
- * Insert a new element to the heap.
+ * Inserts a new element to the heap.
  *
  * returns the added HeapElement.
  */
 const HeapElement *heap_insert(Heap *heap, int key);
 
+/*
+ * Updates the priority of a heap element which
+ * might lead to a internal reorder of the elements.
+ */
 void heap_update_priority(Heap *heap, HeapElement *heap_element, int new_key);
 
 void heap_print(Heap* heap);
